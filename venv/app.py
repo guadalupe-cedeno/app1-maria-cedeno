8e4d9b76f111e35d6e00e471547332783f6af1ef
guadalupe-cedeno <17460080@colima.tecnm.mx>
2021-01-20 01:17:04 -0600

    first commit

---
+++ venv/app.py
@@ -0,0 +1,47 @@
+from flask import  Flask, render_template, url_for, redirect, request
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.debug = True
+Bootstrap(app)
+
+#configuracion para conexion con postgress
+#app.config['SQLALCHEMY_DATABASE_URI']='postgresql+psycopg2://postgres:12345@localhost:5432/escolares'
+app.config['SQLALCHEMY_DATABASE_URI']='postgres:postgres://peounwgkfretfn:925178ef18666e1f4b0ca9221a3c76e083ace5ec9cf2a8e5aa8aef6b1eb2673a@ec2-54-144-45-5.compute-1.amazonaws.com:5432/dbek9bb3cu83a1'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+#Modelo de datos
+class Alumnos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(30))
+    apellido = db.Column(db.String(100))
+
+lista = ["Nosotros", "Contacto", "Preguntas Frecuentes"]
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    print("index")
+    if request.method == "POST":
+        print("request")
+        campo_nombre = request.form['nombre']
+        campo_apellido = request.form['apellido']
+        alumno = Alumnos(nombre=campo_nombre, apellido=campo_apellido)
+        db.session.add(alumno)
+        db.session.commit()
+        mensaje = "Alumno Registrado"
+        return render_template("index.html", mensaje=mensaje)
+    return render_template("index.html", variable=lista)
+        #return redirect('/acerca')
+
+@app.route('/acerca')
+def acerca():
+    print(hola)
+    consulta = Alumnos.query.all()
+    print(consulta)
+    return render_template("acerca.html", variable=consulta)
+
+if __name__ == "__main__":
+    app.run()
